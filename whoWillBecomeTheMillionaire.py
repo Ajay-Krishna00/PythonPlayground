f0179826f89eb2550d4d8156b50caeaa0cdde41c
Ajay-Krishna00 <enthusiastajay00@gmail.com>
2024-06-26 14:15:33 +0530

    Just changed Intentaions

--- whoWillBecomeTheMillionaire.py
+++ whoWillBecomeTheMillionaire.py
@@ -5,8 +5,26 @@ print("\t\t\t","\t*CLAPPING*"*5)
 print("\nWelcome ",name," to \"Who Will Become The Millionaire\"\n")
 print("INSTRUCTIONS\n* There are 5 random questions\n* Answer should be entered in the form of option\n* Each correct answer will give you 10 Million\n* Each wrong answer will deduce 5 million\n\n\tSO LETS BEGIN")
 
-q=["How many sides does an octagon have?","From what country does the chihuahua dog originate?","Who is the Roman goddess of love?","Which side of the brain is responsible for logic and rational thought?","Nyctophobia, Scotophobia, Lygophobia as well as Achluophobia are all the fear of what?","What does the acronym RPM stand for?","What is glass mainly made up of?","Who developed the world's first vaccine?","What is the capital of India?","A cube is cut into two equal halves. How many sides does each cube have?"]
-a=[[8,6,12,10],["Mexico","India","Australia","Canada"],["Venus","Mars","Minerva","Juno"],["left","right","Both left and right","none"],["darkness or night","bacteria","balloon","height or weight"],["Revolutions per minute","Rotations per minute","Reaction power measure","Road Permission Measure"],["Sand","air","plant","water"],["Edward Jenner","Charles Darwin","Carl Linnaeus","Rosalind Franklin"],["Delhi","Bangalore","Kerala","Mumbai"],["5 or 6","1 or 4",12,6]]
+q=[ "How many sides does an octagon have?",
+    "From what country does the chihuahua dog originate?",
+    "Who is the Roman goddess of love?",
+    "Which side of the brain is responsible for logic and rational thought?",
+    "Nyctophobia, Scotophobia, Lygophobia as well as Achluophobia are all the fear of what?",
+    "What does the acronym RPM stand for?",
+    "What is glass mainly made up of?",
+    "Who developed the world's first vaccine?",
+    "What is the capital of India?",
+    "A cube is cut into two equal halves. How many sides does each cube have?"  ]
+a=[     [8,6,12,10],
+        ["Mexico","India","Australia","Canada"],
+        ["Venus","Mars","Minerva","Juno"],
+        ["left","right","Both left and right","none"],
+        ["darkness or night","bacteria","balloon","height or weight"],
+        ["Revolutions per minute","Rotations per minute","Reaction power measure","Road Permission Measure"],
+        ["Sand","air","plant","water"],
+        ["Edward Jenner","Charles Darwin","Carl Linnaeus","Rosalind Franklin"],
+        ["Delhi","Bangalore","Kerala","Mumbai"],
+        ["5 or 6","1 or 4",12,6]    ]
 prizeMoney=0
 def count(a):
     global prizeMoney
